419686f50a046ddb87c40e78db1997c1ffe726ef
Elizabeth <lumban@sg.ibm.com>
2019-04-17 12:55:25 +0800

    update server.js

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ const server = http.createServer(app);
 /**
 * Listen on provided port, on all network interfaces.
 */
-server.listen(port, () => console.log(`API running on localhost:${port}`));
+server.listen(port);
 
 function api(req, res) {
 var strquery = require('./db2function');
